9a43d95cff45bcf1abf9699800737d387bd65704
EugeneAndroschuk <111381321+EugeneAndroschuk@users.noreply.github.com>
2023-05-21 21:11:08 +0300

    ADDED background to home-page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import commonCss from "../styles/common.module.css";
+import css from "./pages.module.css";
 
 const Home = () => {
     return (
-        <div className={commonCss.container}> 
-            <h1>HOME PAGE</h1>
-        </div>)
+        <div className={css['home-bgd']}>
+            <div className={commonCss.container}>
+                {/* <h1>HOME PAGE</h1> */}
+            </div>
+        </div>
+        
+    )
 }
 
 export default Home;
