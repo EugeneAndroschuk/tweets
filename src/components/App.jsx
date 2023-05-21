18ed80eabbf86923d638725dbc8530de75104921
EugeneAndroschuk <111381321+EugeneAndroschuk@users.noreply.github.com>
2023-05-21 20:40:43 +0300

    REMOVED App to components

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import ShareLayout from './ShareLayout';
+import Tweets from 'pages/Tweets';
+import Home from 'pages/Home';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<ShareLayout />} >
+          <Route index element={<Home />} />
+          <Route path="/tweets" element={<Tweets />} />
+          <Route path="*" element={<h1>Not found</h1>} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
